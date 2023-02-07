5b65aa39eca7cc5ae1330f1d8c6df9a92ecf4aa2
issatada <tada.issa61@gmail.com>
2023-02-07 11:17:55 +0900

    第一回修正

--- main2.py
+++
@@ -1,3 +0,0 @@
-from gensim.models import KeyedVectors
-KeyedVectors.load('chive-1.2-mc90.finetuned-mc3.kv')  # Load as KeyedVectors
-Word2Vec.load('chive-1.2-mc90.finetuned-mc3.bin')     # Load as Full model
